3b12309b856f36b566fed50fb56b9516c5ddde6d
Hailskurt <47931581+Hailskurt@users.noreply.github.com>
2021-12-21 02:28:49 +0300

    сделал код более питоническим, добавил шаблонизацию в index.html

--- tours/views.py
+++ tours/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 import tours.data as data
 import random
 
-
 tours = data.tours
 
 
@@ -11,32 +10,25 @@ def main_view(request):
     random_tours = []
     for key in keys:
         random_tours.append(tours[key])
+        random_tours[-1]['key'] = key
     return render(request, 'index.html', {'tours': random_tours,
                                           'title': data.title,
                                           'subtitle': data.subtitle,
                                           'description': data.description,
-                                          'keys': keys})
+                                          'keys': keys,
+                                          'departures': data.departures})
 
 
 def departure_view(request, departure):
     matched_tours = []
-    max_price = 0
-    min_price = float('inf')
-    max_time = 0
-    min_time = float('inf')
     for key in tours.keys():
         if tours[key]['departure'] == departure:
             matched_tours.append(tours[key])
             matched_tours[-1]['key'] = key
-    for tour in matched_tours:
-        if tour['price'] > max_price:
-            max_price = tour['price']
-        if tour['price'] < min_price:
-            min_price = tour['price']
-        if tour['nights'] > max_time:
-            max_time = tour['nights']
-        if tour['nights'] < min_time:
-            min_time = tour['nights']
+    max_time = max(tour['nights'] for tour in matched_tours)
+    min_time = min(tour['nights'] for tour in matched_tours)
+    max_price = max(tour['price'] for tour in matched_tours)
+    min_price = min(tour['price'] for tour in matched_tours)
 
     return render(request, 'departure.html', {'departure': data.departures[departure][3:],
                                               'tours': matched_tours,
@@ -44,8 +36,9 @@ def departure_view(request, departure):
                                               'max_price': max_price,
                                               'min_price': min_price,
                                               'max_time': max_time,
-                                              'min_time': min_time})
+                                              'min_time': min_time,
+                                              'departures': data.departures})
 
 
 def tour_view(request, tour_id):
-    return render(request, 'tour.html', tours[tour_id])
+    return render(request, 'tour.html', {'tour': tours[tour_id]})
