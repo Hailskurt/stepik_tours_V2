290118fdb82efcf91573e8fc576959828da3adcf
Hailskurt <47931581+Hailskurt@users.noreply.github.com>
2021-12-20 22:58:24 +0300

    Update settings.py

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-10cmt28t3(qkobzb^3w$u9=2rcg68dl!6&8-8e&*2agwz!$r0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
